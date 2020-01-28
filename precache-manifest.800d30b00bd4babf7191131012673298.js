864cf950c8791983be91e31a02168e30ae977b9a
ckubec <ckubec@gmail.com>
2020-01-28 00:31:28 -0800

    Updates

---
+++ precache-manifest.800d30b00bd4babf7191131012673298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b5e91ca311d2e3547f6b3b86d761e8",
+    "url": "/vigilant-barnacle/index.html"
+  },
+  {
+    "revision": "818057943b72cf8e6518",
+    "url": "/vigilant-barnacle/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "84ec433359812033dc82",
+    "url": "/vigilant-barnacle/static/css/main.a4b07949.chunk.css"
+  },
+  {
+    "revision": "818057943b72cf8e6518",
+    "url": "/vigilant-barnacle/static/js/2.706aa51c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/vigilant-barnacle/static/js/2.706aa51c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84ec433359812033dc82",
+    "url": "/vigilant-barnacle/static/js/main.8be0810b.chunk.js"
+  },
+  {
+    "revision": "80f7ef15338568712f6c",
+    "url": "/vigilant-barnacle/static/js/runtime-main.852c85ed.js"
+  }
+]);
