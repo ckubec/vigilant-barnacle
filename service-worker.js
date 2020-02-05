153bb354eec3485e4b808599e7a87dc407c02333
ckubec <ckubec@gmail.com>
2020-02-04 19:02:09 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2130243c8931d7b44c952f2940420957.js"
+  "/precache-manifest.6e1695fd577678cf3f3fef8abe8753b7.js"
 );
 
 self.addEventListener('message', (event) => {
